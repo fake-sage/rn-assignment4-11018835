727ac0689fc669f88e84c26892a58ed602ba1bfd
Nii Ohene Omane-Addo <151413334+fake-sage@users.noreply.github.com>
2024-06-17 19:15:28 +0000

    Add files via upload

---
+++ src/FeatureJobsMockUp.jsx
@@ -0,0 +1,21 @@
+ const FeatureJobsMockUp =[
+    {
+        id: 0,
+        backgroundColor : '#1679AB',
+        logo : require('../assets/facbook.png'),
+        role: 'Software Engineer',
+        company : 'facebook',
+        salary: '$180,00',
+        location: 'Accra,Ghana'
+    },
+    {
+        id: 1,
+        backgroundColor : '#102C57',
+        logo : require('../assets/google.png'),
+        role: 'Front-End Developer',
+        company : 'Google',
+        salary: '$195,00',
+        location: 'Kumasi,Ghana'
+    }
+ ]
+ export default FeatureJobsMockUp;
---
+++ src/PopularjobsMockUp.jsx
@@ -0,0 +1,28 @@
+const PopularjobsMockUp =[
+    {
+        id: 0,
+        logo : require('../assets/burgerking.png'),
+        role: 'Jr Executive',
+        company : 'Burger King',
+        salary: '$96,000/y',
+        location: 'Los Angels, US'
+    },
+    {
+        id: 1,
+        logo : require('../assets/music.png'),
+        role: 'Product Manager',
+        company : 'Beats',
+        salary: '$84,000/y',
+        location: 'Florida, US'
+    },
+    {
+        id: 2,
+        logo : require('../assets/facbook.png'),
+        role: 'Product Manager',
+        company : 'Facebook',
+        salary: '$86,000/y',
+        location: 'Florida, US'
+    }
+
+]
+export default PopularjobsMockUp
