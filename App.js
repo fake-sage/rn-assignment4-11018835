a4bc0ea8628b5eb7a4a357df0553357890d16b45
Nii Ohene Omane-Addo <151413334+fake-sage@users.noreply.github.com>
2024-06-17 19:16:19 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from "./src/HomeScreen";
+import LoginIn from "./src/LoginScreen";
+
+
+
+const Stack = createNativeStackNavigator();
+
+
+const App = () => {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="login">
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Home"
+          component={Home}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="login"
+          component={LoginIn}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
+
+
